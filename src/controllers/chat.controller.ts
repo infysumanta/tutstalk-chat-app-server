4f132980753ce19009b72ccf41873e4a97911e58
Sumanta <sumantablog@gmail.com>
2024-04-08 13:58:25 +0530

    Add routes and controllers for chat functionality

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -143,3 +143,28 @@ export const addToGroup = asyncHandler(async (req: Request, res: Response) => {
     res.json(added);
   }
 });
+
+export const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId }
+    },
+    {
+      new: true
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+
+  if (!removed) {
+    res.status(404);
+    throw new Error('Chat Not Found');
+  } else {
+    res.json(removed);
+  }
+});
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import authRouter from './auth.routes';
 import userRouter from './user.routes';
+import chatRoutes from './chat.routes';
+import messageRoutes from './message.routes';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/chats', chatRoutes);
+router.use('/messages', messageRoutes);
 
 export default router;
